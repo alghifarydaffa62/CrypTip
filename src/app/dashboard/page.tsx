83a456901bacfaaa1235e1ed54e036846db400b3
M Daffa A <daffa.alghifary62@gmail.com>
2026-04-26 12:22:16 +0700

    feat: user stats box

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,24 @@
 'use client'
 
 import { useConnection } from "wagmi"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+import TotalEarnedBox from "@/components/TotalEarnedBox"
+import TotalTippedBox from "@/components/TotalTippedBox"
+import useUserStats from "../hooks/useUserStats"
 
 export default function DashboardPage() {
     const { address } = useConnection()
+    const { totalBalance, totalEarned, totalTipped, isLoading } = useUserStats()
 
     return(
         <div>
             <h1>Hello User! {address?.slice(0, 10)}...{address?.slice(-6)}</h1>
+
+            <div className="flex gap-8">
+                <TotalEarnedBox value={totalEarned} isLoading={isLoading}/>
+                <TotalTippedBox value={totalTipped} isLoading={isLoading}/>
+                <TotalBalanceBox value={totalBalance} isLoading={isLoading}/>
+            </div>
         </div>
     )
 }
